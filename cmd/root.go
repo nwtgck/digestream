0b379ba919393719393dd855af2dd73056cb58fd
Ryo Ota <nwtgck@gmail.com>
2019-01-28 20:12:56 +0900

    [refactor] Use tab characters as indent

--- cmd/root.go
+++ cmd/root.go
@@ -1,20 +1,20 @@
 package cmd
 
 import (
-  "crypto/sha256"
-  "github.com/spf13/cobra"
-  "os"
+	"crypto/sha256"
+	"github.com/spf13/cobra"
+	"os"
 )
 
 var RootCmd = &cobra.Command{
-  Use:   os.Args[0],
-  Short: "digestream",
-  Long:  "Dige(st + St)ream",
-  RunE: func(cmd *cobra.Command, args []string) error {
-    return pipeToStdoutAndCalcHash(sha256.New())
-  },
+	Use:   os.Args[0],
+	Short: "digestream",
+	Long:  "Dige(st + St)ream",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return pipeToStdoutAndCalcHash(sha256.New())
+	},
 }
 
 func init() {
-  cobra.OnInitialize()
+	cobra.OnInitialize()
 }
--- cmd/util.go
+++ cmd/util.go
@@ -1,39 +1,39 @@
 package cmd
 
 import (
-  "fmt"
-  "github.com/mattn/go-tty"
-  "hash"
-  "io"
-  "os"
+	"fmt"
+	"github.com/mattn/go-tty"
+	"hash"
+	"io"
+	"os"
 )
 
 func pipeToStdoutAndCalcHash(h hash.Hash) error {
-  // Create a tee from stdin to stdout
-  tee := io.TeeReader(os.Stdin, os.Stdout)
-  // Calculate hash value
-  _, err := io.Copy(h, tee)
-  if err != nil {
-    return err
-  }
+	// Create a tee from stdin to stdout
+	tee := io.TeeReader(os.Stdin, os.Stdout)
+	// Calculate hash value
+	_, err := io.Copy(h, tee)
+	if err != nil {
+		return err
+	}
 
-  // Open tty
-  tty, err := tty.Open()
-  defer tty.Close()
+	// Open tty
+	tty, err := tty.Open()
+	defer tty.Close()
 
-  // Output of hash
-  var hOut io.Writer
-  // If not error
-  if err == nil {
-    // Set hash output as tty
-    hOut = tty.Output()
-  } else {
-    // Set hash output as stderr
-    hOut = os.Stderr
-  }
+	// Output of hash
+	var hOut io.Writer
+	// If not error
+	if err == nil {
+		// Set hash output as tty
+		hOut = tty.Output()
+	} else {
+		// Set hash output as stderr
+		hOut = os.Stderr
+	}
 
-  // Print hash value
-  fmt.Fprintf(hOut, "%x\n", h.Sum(nil))
+	// Print hash value
+	fmt.Fprintf(hOut, "%x\n", h.Sum(nil))
 
-  return nil
+	return nil
 }
